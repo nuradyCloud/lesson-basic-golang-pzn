8170f1c076e4b62d29220a43631750863b1abed3
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-06-14 22:33:42 +0700

    type data string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Nur")
+	fmt.Println("Nur Ady")
+	fmt.Println("Nur Ady Pamungkas")
+
+	fmt.Println(len("Nur"))
+	fmt.Println("Nur Ady"[0])
+	fmt.Println("Nur Ady Pamungkas"[1])
+}
