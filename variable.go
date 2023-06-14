368bd3978caad8ae2a05aa0ff7a0ac9114ff25ab
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-06-14 22:46:44 +0700

    used variable

---
+++ variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Nur Ady"
+	fmt.Println(name)
+
+	name = "Nur Pamungkas"
+	fmt.Println(name)
+
+	var friendName = "Reyga"
+	fmt.Println(friendName)
+
+	var age = 30
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Nur"
+		lastName  = "Pamungkas"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
