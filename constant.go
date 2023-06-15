467950e0440533b770d7dea94d2ab7a994d56775
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-06-15 21:33:29 +0700

    used constant

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstName string = "Nur"
+		lastName  string = "Pamungkas"
+		value            = 1000
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
