f4e658ff8de748a4eb721caf02e44ffee4a86227
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-07-13 14:58:37 +0700

    type assertions

---
+++ type_assertions.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func random() interface{} {
+	//return "Ups"
+	return true
+}
+
+func main() {
+	var result interface{} = random()
+	//var resultString = result.(string)
+	//fmt.Println(resultString)
+
+	switch value := result.(type) {
+	case string:
+		fmt.Println("Value", value, "is string")
+	case int:
+		fmt.Println("Value", value, "is int")
+	default:
+		fmt.Println("Unknown type")
+	}
+}
