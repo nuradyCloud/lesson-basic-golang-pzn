7f8dcff5a8e9259d55898a13ece2f0fd0ff19eb0
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-07-22 00:09:21 +0700

    using pointer

---
+++ pointer.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	var address1 Address = Address{"Jakarta Barat", "DKI Jakarta", "Indonesia"}
+	//address2 := address1 // before pointer pass by value
+	var address2 *Address = &address1 //after pass by reference
+	var address3 *Address = &address1
+
+	address2.City = "Semarang"
+
+	*address2 = Address{"Bandung", "Jawa Barat", "Indonesia"}
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+	fmt.Println(address3)
+
+	var address4 *Address = new(Address)
+	address4.City = "Malang"
+	fmt.Println(address4)
+}
